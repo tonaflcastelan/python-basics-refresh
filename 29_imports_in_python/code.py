69f5735e09b26ea7bcfd5f6ba9aaca37dac9e5a1
tonacastelan <tonatiuh.flores@konfio.mx>
2019-10-15 14:49:38 -0500

    Python basics: imports, typ hinting

---
+++ 29_imports_in_python/code.py
@@ -0,0 +1,2 @@
+# Importing
+import my_module
---
+++ 30_relative_imports_python/libs/mylib.py
@@ -0,0 +1,8 @@
+  
+from libs.operations import operator
+
+print("mylib.py:", __name__)
+
+# -- can do relative imports from file with parent package --
+
+from .operations import operator
